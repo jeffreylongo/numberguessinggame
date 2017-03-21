73825be550b7fb5cb8fd3d9be5461a03054c8eba
Jeff Longo <jeffreylongo@yahoo.com>
2017-03-21 14:50:29 -0400

    basic number game. doesnt feed 2nd guess back through

--- numberguessinggame/numberguessinggame/Program.cs
+++ numberguessinggame/numberguessinggame/Program.cs
@@ -10,13 +10,51 @@ namespace numberguessinggame
     {
         static void Main(string[] args)
         {
-            start:
+            
+            var guess = 0;
             Random rnd = new Random();
-            int num01 = rnd.Next(100);
+            int num01 = rnd.Next(1,100);
+            var counter = 0;
 
+            //start:
             Console.WriteLine(num01);
-            Console.ReadLine();
-            goto start;
+            Console.WriteLine("Guess a number 1 - 100...or else...");
+            //var rawInput = var.TryParse(rawInput, out guess);
+            var rawInput = Console.ReadLine();
+            var number = int.TryParse(rawInput, out guess);
+
+
+            if (guess == num01)
+            {
+                Console.WriteLine("You got a Bingo!");
+                Console.ReadLine();
+
+            }
+            else if ( guess > num01)
+            {
+                Console.WriteLine("Too high, ya big dummy! Try again...");
+                Console.ReadLine();
+                counter++;
+                if (counter == 5)
+                {
+                    Console.WriteLine("You Lose!");
+                }
+
+            }
+            else if ( guess < num01)
+            {
+                Console.WriteLine("As Little John would say...Too LOW! Try again...");
+                Console.ReadLine();
+                counter++;
+                if (counter == 5)
+                {
+                    Console.WriteLine("You Lose!");
+                }
+            }
+            
+            //Console.WriteLine(num01);
+            //Console.ReadLine();
+            //goto start;
         }
     }
 }

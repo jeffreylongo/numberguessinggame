1d607b0743f0eda8cd6e25c584035b3cd86c1478
Jeff Longo <jeffreylongo@yahoo.com>
2017-03-21 13:41:37 -0400

    added number gen code

---
+++ numberguessinggame/numberguessinggame/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberguessinggame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start:
+            Random rnd = new Random();
+            int num01 = rnd.Next(100);
+
+            Console.WriteLine(num01);
+            Console.ReadLine();
+            goto start;
+        }
+    }
+}
